f255114adcb33c196e91493d5d42f41827f207f4
yasmeen10 <yasmeenwaleed844@gmail.com>
2024-05-31 00:49:20 +0300

    update orderSchema

--- BackEnd/models/Order.js
+++ BackEnd/models/Order.js
@@ -1,3 +1,4 @@
+const { required } = require("joi");
 const mongoose = require("mongoose");
 
 const orderSchema = mongoose.Schema({
@@ -6,13 +7,23 @@ const orderSchema = mongoose.Schema({
     ref: "User",
     required: true,
   },
+  firstName: {
+    type: String,
+    minLength: [3, "First name must be at least 3 characters long"],
+    required: [true, "First name is required"],
+  },
+  lastName: {
+    type: String,
+    minLength: [3, "Last name must be at least 3 characters long"],
+  },
+  email: {
+    type: String,
+    minLength: [3, "Email must be at least 3 characters long"],
+    required: [true, "Email is required"],
+    unique: true,
+  },
   totalPrice: {
     type: Number,
-  },
-  status: {
-    type: String,
-    enum: ["Pending", "Accepted", "Canceled"],
-    default: "Pending",
   },
   phoneNumber: {
     type: String,
@@ -20,6 +31,19 @@ const orderSchema = mongoose.Schema({
   address: {
     type: String,
     minLength: [3, "Address must be at least 3 characters long"],
+  },
+  country: {
+    type: String,
+    required: [true, "Country is required"],
+  },
+  city: {
+    type: String,
+    required: [true, "City is required"],
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "Accepted", "Canceled"],
+    default: "Pending",
   },
   dateOfOrder: {
     type: Date,
