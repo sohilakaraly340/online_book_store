f7d1743cb55036b1b6b92c2c7c62550c43b643c2
mariam <mariamelmsery7@gmail.com>
2024-06-11 23:31:17 +0300

    FeaturePagination

--- BackEnd/routes/admin/User.js
+++ BackEnd/routes/admin/User.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const { handleAsync } = require("../../Errors/HandleAsync");
 const { admin } = require("../../middlewares/Admin");
+const paginate = require("../../middlewares/Pagination");
 const router = express.Router();
-
+const users = require("../../models/User")
 const userRouter = (userController) => {
   router.get(
     "/",
-    admin,
+    admin,paginate(users),
     handleAsync(async (req, res) => {
       const allUser = await userController.getAllUser();
-      res.status(200).json({ success: true, data: allUser });
+      res.status(200).json({ success: true, data: req.paginatedResult });
     })
   );
 
